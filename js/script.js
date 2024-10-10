2bcacc8f135d71161ba116157c6a29c9aa8b07f9
Kekensya <muh.kensya081205@gmail.com>
2024-10-10 16:53:22 +0700

    checkpoint 2

--- js/script.js
+++ js/script.js
@@ -2,7 +2,7 @@ document.getElementById('convert-btn').addEventListener('click', function() {
     const celsius = parseFloat(document.getElementById('celsius').value);
     const fahrenheit = (celsius * 1.8) + 32;
     document.getElementById('fahrenheit').value = fahrenheit.toFixed(2);
-    document.getElementById('cara-kalkulasi').value = `${celsius} C * (9/5) + 32 = ${fahrenheit.toFixed(2)} F`;
+    document.getElementById('cara-kalkulasi').value = `${celsius}°C * (9/5) + 32 = ${fahrenheit.toFixed(2)}°F`;
 });
 
 document.getElementById('reset-btn').addEventListener('click', function() {
@@ -12,8 +12,36 @@ document.getElementById('reset-btn').addEventListener('click', function() {
 });
 
 document.getElementById('reverse-btn').addEventListener('click', function() {
-    const fahrenheit = parseFloat(document.getElementById('fahrenheit').value);
-    const celsius = (fahrenheit - 32) / 1.8;
-    document.getElementById('celsius').value = celsius.toFixed(2);
-    document.getElementById('cara-kalkulasi').value = `${fahrenheit.toFixed(2)} F - 32 = ${celsius.toFixed(2)} C / (9/5)`;
+    const tempCelsius = document.getElementById('celsius');
+    const tempFahrenheit = document.getElementById('fahrenheit');
+    const tempCalculation = document.getElementById('cara-kalkulasi');
+    const tempLabelCelsius = document.getElementById('label-celsius');
+    const tempLabelFahrenheit = document.getElementById('label-fahrenheit');
+
+    if (tempCelsius.value && tempFahrenheit.value) {
+       
+        [tempCelsius.value, tempFahrenheit.value] = [tempFahrenheit.value, tempCelsius.value];
+
+        // update cara-kalkulasi
+        if (tempLabelCelsius.textContent === 'Celcius') {
+            tempCalculation.value = `${tempCelsius.value}°F * (5/9) - 32 = ${tempFahrenheit.value}°C`;
+        } else {
+            tempCalculation.value = `${tempCelsius.value}°C * (9/5) + 32 = ${tempFahrenheit.value}°F`;
+        }
+
+        // Tukar  label
+
+        [tempLabelCelsius.textContent, tempLabelFahrenheit.textContent] = [tempLabelFahrenheit.textContent, tempLabelCelsius.textContent];
+    }
+});
+
+const conversionText = document.querySelector('p[style="color: rgb(255, 40, 40);font-size: 18px;"]');
+const reverseButton = document.getElementById('reverse-btn');
+
+reverseButton.addEventListener('click', () => {
+    if (conversionText.textContent === 'Celcius ke Fahrenheit') {
+        conversionText.textContent = 'Fahrenheit ke Celcius';
+    } else {
+        conversionText.textContent = 'Celcius ke Fahrenheit';
+    }
 });
